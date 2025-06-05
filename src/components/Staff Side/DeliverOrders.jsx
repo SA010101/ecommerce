d968a57adec8f02a79c5b8c62473a845626f70ae
shaan Aslam <shaanaslam0101@gmail.com>
2025-06-05 19:02:56 +0500

    update

--- src/components/Staff Side/DeliverOrders.jsx
+++ src/components/Staff Side/DeliverOrders.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 function DeliverOrders() {
   return (
     <div>
-        Deliver
+        Deliver Orders
     </div>
   )
 }
--- src/components/Staff Side/DispatchOrders.jsx
+++ src/components/Staff Side/DispatchOrders.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 function DispatchOrders() {
   return (
     <div>
-      Dispatch
+      Dispatch Orders
     </div>
   )
 }
